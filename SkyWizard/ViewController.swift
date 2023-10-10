1f80e2bfff04be2f9df57da02ba4cdf4d2b2af37
HishD <hisharadilshan3@gmail.com>
2023-10-10 14:00:33 +0100

    Initial Commit

---
+++ SkyWizard/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  SkyWizard
+//
+//  Created by Hishara Dilshan on 2023-10-10.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
