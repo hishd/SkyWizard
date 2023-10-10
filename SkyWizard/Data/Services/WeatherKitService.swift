5288745f21ba054a0a20bf567ef5641b513857ca
HishD <hisharadilshan3@gmail.com>
2023-10-11 00:40:15 +0100

    Added weather kit features

--- SkyWizard/Data/Services/WeatherKitService.swift
+++ SkyWizard/Data/Services/WeatherKitService.swift
@@ -8,10 +8,31 @@
 import Foundation
 import WeatherKit
 import CoreLocation
+import OSLog
 
+final class WeatherKitService {
+    
+    let service = WeatherService.shared
+    
+    public func getWeather(for location: CLLocation) async throws -> WeatherResult {
+        do {
+            let result = try await service.weather(for: location)
+            return WeatherResult(
+                currentWeather: result.currentWeather,
+                hourlyForecaset: result.hourlyForecast,
+                dailyForecast: result.dailyForecast
+            )
+        } catch {
+            Logger.viewCycle.error("WeatherKit Error : \(error.localizedDescription)")
+            throw ApplicationError.weatherKitError("Could not fetch weather data. Please try again")
+        }
+    }
+}
 
-final class WeatherKitService {        
-    public func getWeather(for location: CLLocation) {
-        
+extension WeatherKitService {
+    public struct WeatherResult {
+        let currentWeather: CurrentWeather
+        let hourlyForecaset: Forecast<HourWeather>
+        let dailyForecast: Forecast<DayWeather>
     }
 }
