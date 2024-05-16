c5a0016e00374bcff480f0d2a966cbd38a19159c
HishD <hisharadilshan3@gmail.com>
2024-05-16 19:12:45 +0100

    Added WeatherService abstraction

--- SkyWizard/Data/Services/OpenMetroWeatherService.swift
+++ SkyWizard/Data/Services/OpenMetroWeatherService.swift
@@ -8,8 +8,15 @@
 import Foundation
 import CoreLocation
 import OSLog
+import Combine
 
-final class WeatherService {
+final class OpenMetroWeatherService: WeatherService {
+    func getWeather(for location: CLLocation) -> Future<WeatherResult, WeatherError> {
+        return Future { promise in
+            promise(.success(WeatherResult()))
+        }
+    }
+    
     public func getWeather(for location: CLLocation) async throws -> WeatherResult {
         return WeatherResult()
     }
